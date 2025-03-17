a1e16d53f56757aead4e9158f4bd3db8e2477d07
kemsguy7 <mattidungafa@gmail.com>
2025-03-17 15:39:00 +0100

    updates: fixed aligment of footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <Flex
-      gap='3'
       as='footer'
       width='100%'
       align='center'
-      justify='between'
-      className='border-t-1 border-primary p-4 items-center h-18'
+      justify='center'
+      className='border-t border-primary p-4 items-center h-18'
     >
       <Box>
         <Text className='text-primary text-xl' as='span' role='img' aria-label='logo'>
